8e3123627aea7888a3cab90fbd70a32e904b60ec
IDO <susususu07@naver.com>
2024-05-06 04:35:57 +0900

    Add files via upload

---
+++ booleanTest.js
@@ -0,0 +1,14 @@
+// Falsy 값
+console.log(Boolean(false));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(0));
+console.log(Boolean(''));
+
+// Truthy 값
+console.log(Boolean(true));
+console.log(Boolean('codeit'));
+console.log(Boolean(123));
+console.log(Boolean(-123));
+console.log(Boolean({}));
