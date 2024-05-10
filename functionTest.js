bc9eef35a561c9f0d4a664d2900ff3204ba62b8e
IDO <susususu07@naver.com>
2024-05-11 00:49:58 +0900

    Add files via upload

---
+++ functionTest.js
@@ -0,0 +1,14 @@
+// 함수를 만드는 방법
+// 1. 함수 선언(Function Declaration)
+function printCodeit1() {
+  console.log('Coodeit');
+}
+
+printCodeit1();
+
+//2. 함수 표현식(Function Expression)
+const printCodeit2 = function () {
+  console.log('Coodeit');
+};
+
+printCodeit2();
