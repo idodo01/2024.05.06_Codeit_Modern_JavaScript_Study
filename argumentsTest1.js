c811741b5ea7490c7693054900aa0c3c9bd97648
IDO <susususu07@naver.com>
2024-05-14 23:54:39 +0900

    Add files via upload

---
+++ argumentsTest1.js
@@ -0,0 +1,11 @@
+// a, b, c 는 parameter
+function printArguments(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+
+// "나는", "아이두", "입니다" 는 argument
+printArguments("나는", "아이두", "입니다");
+
