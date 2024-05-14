e490cc472b69f3b3de074a125756f8ec3aa14729
IDO <susususu07@naver.com>
2024-05-14 23:48:49 +0900

    Add files via upload

---
+++ argumentsTest.js
@@ -0,0 +1,20 @@
+function firstWords() {
+  let word = '';
+  for(let arg of arguments) {
+    word+=arg[0];
+  }
+  console.log(word);
+}
+
+firstWords('나만', '없어', '고양이');
+firstWords('아니', '바나나말고', '라면먹어');
+firstWords('만두', '반으로', '잘라먹네', '부지런하다');
+firstWords('결국', '자바스크립트가', '해피한', '지름길');
+firstWords('빨간색', '주황색', '노란색', '초록색', '파란색', '남색', '보라색');
+
+
+// 나없고
+// 아바라
+// 만반잘부
+// 결자해지
+// 빨주노초파남보
