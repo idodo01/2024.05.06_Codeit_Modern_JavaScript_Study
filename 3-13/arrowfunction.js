fbb29ef7e689678493012ebe228e3a679d85599e
IDO <susususu07@naver.com>
2024-09-23 13:23:57 +0900

    Add files via upload

---
+++ 3-13/arrowfunction.js
@@ -0,0 +1,9 @@
+// Arrow function
+const getTwice1 = function(number) {
+    return number * 2;
+  };
+  
+  const getTwice2 = number => nubmer * 2;
+  
+  console.log(getTwice1(6));
+  console.log(getTwice2(6));
