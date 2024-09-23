76eac7df429c308ed0c597899cc45ed270fd4594
IDO <susususu07@naver.com>
2024-09-23 13:49:45 +0900

    Add files via upload

---
+++ 3-13/arrowfunction2.js
@@ -0,0 +1,14 @@
+// Arrow function: 이름없는 익명함수
+
+// 파라미터가 하나일 경우,
+// () 생략 가능 
+// but, () 권장
+const getTwice = number => {
+  return number * 2;
+};
+
+// 파라미터가 둘 이상일 경우,
+// () 필수
+const sum = (a,b) => {
+  return a + b;
+};
