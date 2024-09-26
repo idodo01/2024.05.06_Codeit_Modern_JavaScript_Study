09f30af3b2c21cfc55c8a9eb19d0bc9d9478c657
IDO <susususu07@naver.com>
2024-09-27 01:04:15 +0900

    Add files via upload

---
+++ 4_13/destructuringTest2.js
@@ -0,0 +1,54 @@
+// 1. Destructuring 문법을 사용해서 title, artist, year, medium 변수에 myBestArt 객체의 각 프로퍼티를 할당해 주세요
+const myBestArt = {
+	title: '별이 빛나는 밤에',
+	artist: '빈센트 반 고흐',
+	year: 1889,
+	medium: '유화',
+};
+
+var {title, artist, year, medium} = myBestArt;
+
+
+// 2. Destructuring 문법을 활용해서 myBestSong의 프로퍼티 중 title과 artist는 각각 songName과 singer라는 변수에, 나머지는 rest라는 변수에 객체로 할당해 주세요
+const myBestSong = {
+	title: '무릎',
+	artist: '아이유(IU)',
+	release: '2015.10.23.',
+	lyrics: '모두 잠드는 밤에...'
+};
+
+var {title : songName, artist : singer, ...rest} = myBestSong;
+
+// 3. printMenu 함수 안에 잘못 작성된 Destructuring 코드를 수정해 주세요
+const menu1 = { name: '아메리카노' };
+const menu2 = { name: '바닐라 라떼', ice: true };
+const menu3 = { name: '카페 모카', ice: false };
+
+function printMenu(menu) {
+	//  menu 파라미터로 전달되는 객체에 ice 프로퍼티가 없을 경우 기본값은 true여야 합니다
+	const {name, ice = true} = { ...menu };
+
+	console.log(`주문하신 메뉴는 '${ice ? '아이스' : '따뜻한'} ${name}'입니다.`);
+}
+
+
+
+// 테스트 코드
+console.log(title); // 별이 빛나는 밤에
+console.log(artist); // 빈센트 반 고흐
+console.log(year); // 1889
+console.log(medium); // 유화
+console.log(songName); // 무릎
+console.log(singer); // 아이유(IU)
+console.log(rest); // { release: '2015.10.23.', lyrics: '모두 잠드는 밤에...' }
+printMenu(menu1); // 주문하신 메뉴는 '아이스 아메리카노'입니다.
+printMenu(menu2); // 주문하신 메뉴는 '아이스 바닐라 라떼'입니다.
+printMenu(menu3); // 주문하신 메뉴는 '따뜻한 카페 모카'입니다.
+
+
+
+
+
+
+
+
