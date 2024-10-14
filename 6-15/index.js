f1219eb3cdfe6e68cf8caefbbd6b5d6473c3d7dd
IDO <susususu07@naver.com>
2024-10-14 14:30:26 +0900

    Add files via upload

---
+++ 6-15/index.js
@@ -0,0 +1,6 @@
+// import { default as printerJS } from './printer.js';
+import printerJS from './printer.js';
+
+const title = "CodeitPrinter";
+
+printerJS(title); // CodeitPrinter
---
+++ 6-15/printer.js
@@ -0,0 +1,7 @@
+const title = 'CodeitPrinter';
+
+function print(value) {
+  console.log(value);
+}
+
+export default print;
